14b0a5b13c15b4fefc044a22bb06781add28821a
Heku Watada <heku.watada@gmail.com>
2019-12-08 21:23:54 +0000

    [6.2] Implemented double() using nonNegativeInt().

--- src/main/scala/ch6/rng/rngClients.scala
+++ src/main/scala/ch6/rng/rngClients.scala
@@ -5,4 +5,9 @@ object rngClients {
     val (int, nextRng) = rng.nextInt
     (if (int < 0) -(int + 1) else int, nextRng)
   }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (int, nextRng) = nonNegativeInt(rng)
+    (int / (Int.MaxValue.toDouble + 1), nextRng)
+  }
 }
--- src/test/scala/ch6/rng/rngClientsSpec.scala
+++ src/test/scala/ch6/rng/rngClientsSpec.scala
@@ -1,6 +1,6 @@
 package ch6.rng
 
-import ch6.rng.rngClients.nonNegativeInt
+import ch6.rng.rngClients._
 import org.mockito.Mockito.{reset, when}
 import org.scalatest.{BeforeAndAfter, FunSpec, Matchers}
 import org.scalatestplus.mockito.MockitoSugar
@@ -14,7 +14,7 @@ class rngClientsSpec extends FunSpec with Matchers with BeforeAndAfter with Mock
     reset(mockRng)
   }
 
-  describe("nonNegativeInt()") {
+  describe("nonNegativeInt() - generates a random integer between 0 and Int.maxValue") {
     it("returns a zero when RNG returns a zero") {
       when(mockRng.nextInt).thenReturn((0, newRng))
       nonNegativeInt(mockRng) shouldBe (0, newRng)
@@ -38,6 +38,42 @@ class rngClientsSpec extends FunSpec with Matchers with BeforeAndAfter with Mock
     it("return a positive integer when RNG returns min negative integer") {
       when(mockRng.nextInt).thenReturn((Int.MinValue, newRng))
       nonNegativeInt(mockRng) shouldBe (Int.MaxValue, newRng)
+    }
+  }
+
+  describe("double() - generates a Double between 0 and 1 (exclusive)") {
+    it("returns a zero when RNG returns a zero") {
+      when(mockRng.nextInt).thenReturn((0, newRng))
+      double(mockRng) shouldBe (0.0, newRng)
+    }
+
+    it("returns a zero int when RNG returns -1") {
+      when(mockRng.nextInt).thenReturn((-1, newRng))
+      double(mockRng) shouldBe (0.0, newRng)
+    }
+
+    it("returns 0 < y < 1 when RNG returns a random negative integer x") {
+      when(mockRng.nextInt).thenReturn((-123456, newRng))
+      val (actualInt, actualRng) = double(mockRng)
+      actualRng shouldBe newRng
+      actualInt should be > 0.0
+      actualInt should be < 1.0
+    }
+
+    it("returns 0 < y < 1 when RNG returns min integer") {
+      when(mockRng.nextInt).thenReturn((Int.MinValue, newRng))
+      val (actualInt, actualRng) = double(mockRng)
+      actualRng shouldBe newRng
+      actualInt should be > 0.0
+      actualInt should be < 1.0
+    }
+
+    it("returns 0 < y < 1 when RNG returns max integer") {
+      when(mockRng.nextInt).thenReturn((Int.MaxValue, newRng))
+      val (actualInt, actualRng) = double(mockRng)
+      actualRng shouldBe newRng
+      actualInt should be > 0.0
+      actualInt should be < 1.0
     }
   }
 }
