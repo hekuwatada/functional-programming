e4de62ecee623605aed6d5f9fbb6abb760ca68b3
Heku Watada <heku.watada@gmail.com>
2019-12-15 02:19:07 +0000

    [6.11] Implemented flatMap() in State.

--- src/main/scala/ch6/state/State.scala
+++ src/main/scala/ch6/state/State.scala
@@ -2,6 +2,10 @@ package ch6.state
 
 // State action/transition
 case class State[S, +A](run: S => (A, S)) {
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State({ s =>
+    val (a, nextS) = run(s)
+    f(a).run(nextS)
+  })
 
   // TODO: implement map, map2, flatMap
 }
--- src/test/scala/ch6/state/StateSpec.scala
+++ src/test/scala/ch6/state/StateSpec.scala
@@ -1,17 +1,39 @@
 package ch6.state
 
 import ch6.rng.RNG
-import org.mockito.Mockito.verifyNoInteractions
-import org.scalatest.{FunSpec, Matchers}
+import org.mockito.Mockito._
+import org.scalatest.{BeforeAndAfter, FunSpec, Matchers}
 import org.scalatestplus.mockito.MockitoSugar
 
-class StateSpec extends FunSpec with Matchers with MockitoSugar {
+class StateSpec extends FunSpec with Matchers with MockitoSugar with BeforeAndAfter {
+  val rng = mock[RNG]
+
+  before {
+    reset(rng)
+  }
 
   describe("unit()") {
     it("returns state transition as is") {
-      val rng = mock[RNG]
       State.unit[RNG, Int](123).run(rng) shouldBe (123, rng)
       verifyNoInteractions(rng)
+    }
+  }
+
+  describe("flatMap()") {
+    it("maps and flattens") {
+      val nextRng1 = mock[RNG]
+      val nextRng2 = mock[RNG]
+
+      when(rng.nextInt).thenReturn((1, nextRng1))
+      when(nextRng1.nextInt).thenReturn((2, nextRng2))
+
+      val state = State[RNG, Int](_.nextInt)
+      val f: Int => State[RNG, Double] = i => State(s => {
+        val (newInt, newS) = s.nextInt
+        (i.toDouble + newInt, newS)
+      })
+      state.flatMap(f).run(rng) shouldBe (3.0, nextRng2)
+      verifyNoInteractions(nextRng2)
     }
   }
 }
