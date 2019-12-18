4e573415e0e7fc39589c3150c2347afe6a93d36e
Heku Watada <hekuwatada@gmail.com>
2019-12-18 22:45:19 +0000

    [6.12a] Added definition of modify() to be implemented.

--- src/main/scala/ch6/state/State.scala
+++ src/main/scala/ch6/state/State.scala
@@ -9,13 +9,21 @@ case class State[S, +A](run: S => (A, S)) {
 
   def map[B](f: A => B): State[S, B] = flatMap(f.andThen(State.unit))
 
+  // flatMap[B](f: A => State[S, B]): State[S, B]
+  // map[B](f: A => B): State[S, B]
+  //TODO: implement map2() in terms of flatMap() and map()
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = State({ s =>
     val (a, nextAS) = run(s)
     val (b, nextBS) = sb.run(nextAS)
     (f(a, b), nextBS)
   })
+
+  def modify(f: S => S): State[S, Unit] = ???
 }
 
 object State {
   def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 }
+
+//TODO: [6.12a] write flatMap x 2 + map x 1 => for comprehension
+//TODO: [6.12] implement get and set
